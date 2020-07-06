f16934178d92e871e91795bcc27b6c141686bd2f
郭新雨 <guoxinyu1992@sina.com>
2020-07-07 02:30:03 +0800

    test: 判斷婚姻是否穩定

--- src/test/graph/galeShapley/index.test.ts
+++ src/test/graph/galeShapley/index.test.ts
@@ -89,24 +89,110 @@ function findResultPersonInfo(matching: Matching, persons: MatingPerson[]): [Mat
   return result;
 }
 
-// /**
-//  * @description 判斷組合是否穩定
-//  * @author 郭新雨
-//  * @date 2020-07-06
-//  * @param {Matching} matching
-//  * @param {MatingPerson[]} persons
-//  * @returns {boolean}
-//  */
-// function matchingIsStable(matching: Matching, persons: MatingPerson[]): boolean {
-//   for (let couple of matching) {
-//     const male = persons.find(ele => ele.id === couple[0]);
-//     const female = persons.find(ele => ele.id === couple[1]);
-//     if (!male || !female) {
-//       continue;
-//     }
-//     // const
-//   }
-// }
+/**
+ * @description
+ * @author 郭新雨
+ * @date 2020-07-07
+ * @param {PersonId} personId
+ * @param {[PersonId,PersonId]} match
+ * @returns {PersonId}
+ */
+function getCoupleIdFromMatching(personId: PersonId, match: [PersonId, PersonId]): PersonId {
+  if (match[0] === personId) {
+    return match[1];
+  } else if (match[1] === personId) {
+    return match[0];
+  }
+  return '';
+}
+
+/**
+ * @description
+ * @author 郭新雨
+ * @date 2020-07-07
+ * @param {PersonId} aId
+ * @param {PersonId} bId
+ * @param {MatingPerson[]} persons
+ * @param {Matching} matching
+ * @returns {boolean}
+ */
+function bLoveABetter(aId: PersonId, bId: PersonId, persons: MatingPerson[], matching: Matching):
+boolean {
+  const lover = persons.find(ele => ele.id === bId);
+  if (!lover) {
+    return false;
+  }
+  // const aId;
+  const inLoverRank = lover.lovingRank.findIndex(ele => ele === aId);
+  if (inLoverRank < 0) {
+    return false;
+  }
+  const loverCouples = matching.find(ele => ele.includes(bId));
+  if (!loverCouples) {
+    return true;
+  }
+  const loverCoupleId = getCoupleIdFromMatching(bId, loverCouples);
+  if (!loverCoupleId) {
+    return true;
+  }
+  const loverCoupleInLoverRank = lover.lovingRank.findIndex(ele => ele === loverCoupleId);
+  return inLoverRank < loverCoupleInLoverRank;
+}
+
+/**
+ * @description 判斷一對夫婦是否穩定，看夫婦雙方相較與自己的配偶，會不會跟别人好上，
+ * 即看是否存在這對夫婦之外的人甲，這對夫婦中的一個人乙相較于自己的配偶更喜歡甲，甲相較于自己的配偶也更喜歡這對夫婦中的那個乙
+ * @author 郭新雨
+ * @param matching
+ * @date 2020-07-06
+ * @param {[MatingPerson, MatingPerson]} couple
+ * @param {MatingPerson[]} persons
+ * @returns {boolean}
+ */
+function coupleIsStable(couple: [MatingPerson, MatingPerson], persons: MatingPerson[], matching: Matching): boolean {
+  for (let personId of couple[0].lovingRank) {
+    if (personId === couple[1].id) {
+      break;
+    }
+    const loverLoveMeBetter = bLoveABetter(couple[0].id, personId, persons, matching);
+    if (loverLoveMeBetter) {
+      return false;
+    }
+  }
+  for (let personId of couple[1].lovingRank) {
+    if (personId === couple[0].id) {
+      break;
+    }
+    const loverLoveMeBetter = bLoveABetter(couple[1].id, personId, persons, matching);
+    if (loverLoveMeBetter) {
+      return false;
+    }
+  }
+  return true;
+}
+
+/**
+ * @description 判斷組合是否穩定
+ * @author 郭新雨
+ * @date 2020-07-06
+ * @param {Matching} matching
+ * @param {MatingPerson[]} persons
+ * @returns {boolean}
+ */
+function matchingIsStable(matching: Matching, persons: MatingPerson[]): boolean {
+  for (let couple of matching) {
+    const male = persons.find(ele => ele.id === couple[0]);
+    const female = persons.find(ele => ele.id === couple[1]);
+    if (!male || !female) {
+      continue;
+    }
+    const isStable = coupleIsStable([male, female], persons, matching);
+    if (!isStable) {
+      return false;
+    }
+  }
+  return true;
+}
 
 /**
  * @description
@@ -130,7 +216,7 @@ export default function testGaleShapley(): void{
     performance.mark('B');
     performance.measure('婚姻匹配所耗時間', 'A', 'B');
 
-    assert.ok(result);
+    assert.ok(matchingIsStable(result, persons));
     done();
   });
 }
